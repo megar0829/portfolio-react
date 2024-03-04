96515c4d62b265a37a1378cd9366100376247dab
megar0829 <megar0829@gmail.com>
2024-03-04 11:28:14 +0900

    :sparkle: Header 작업 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from "react"
+
+const headerNav = [
+   {
+    title: "intro",
+    url: "#intro"
+   },
+   {
+    title: "skill",
+    url: "#skill"
+   },
+   {
+    title: "site",
+    url: "#site"
+   },
+   {
+    title: "portfolio",
+    url: "#port"
+   },
+   {
+    title: "contact",
+    url: "#contact"
+   }
+]
 
 function Header() {
-  return (
-    <div>Header</div>
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
+  return (                                                                  
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header__logo">
+          <h1>
+            <a href="/">portfolio <em>react.js</em></a>
+          </h1>
+        </div>
+        <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+          <ul>
+            {headerNav.map((nav, index) => (
+              <li key={index}>
+                <a href={nav.url}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded={show ? "true" : "false"}
+          aria-label="토글 버튼"
+          role="button"
+          tabIndex="0"
+          onClick={toggleMenu}
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
   )
 }
 
